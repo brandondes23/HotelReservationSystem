a7096ad53c0635f63ee3c1ac8bce50dfe137ffeb
Brandon Despe <brandon.despe@marquette.edu>
2023-11-30 18:20:32 -0600

    Add project files.

---
+++ HotelReservationSystem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HotelReservationSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

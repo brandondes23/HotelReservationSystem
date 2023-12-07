242a5eb73889c213ac6a6571b0202eb90bf4c8a8
Brandon Despe <brandon.despe@marquette.edu>
2023-12-07 13:38:54 -0600

    Created Billing Room and Room Inventory

---
+++ HotelReservationSystem/Billing.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationSystem;
+
+public class Billing
+{
+    public int ReservationId { get; set; }
+    public double Charges { get; set; }
+
+
+    public Billing(int reservationId, double charges)
+    {
+        ReservationId = reservationId;
+        Charges = charges;
+    }
+}

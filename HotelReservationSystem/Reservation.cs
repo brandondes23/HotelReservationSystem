e47f019a2f067eec0994db80448fd13d0f17cbb8
Anastasija Svetnik <anastasija.svetnik@marquette.edu>
2023-12-07 11:51:29 -0600

    git commit -m "Reservation"

--- HotelReservationSystem/Reservation.cs
+++ HotelReservationSystem/Reservation.cs
@@ -1,12 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace HotelReservationSystem
 {
-    internal class Reservation
+    record Reservation(int ReservationID, string FirstName, string LastName, string Email)
     {
+        private readonly global::System.Int32 age;
+        private readonly global::System.String phone;
+
+        public System.Int32 Age => age;
+
+        public System.String Phone => phone;
+
+        public Reservation(int reservationID, string firstName, string lastName, int age, string phone, string email) : this(reservationID, firstName, lastName, email)
+        {
+            SetAge(age);
+            SetPhone(phone);
+        }
+
+        public override string ToString => $"Reservation ID: {ReservationID}, Guest: {FirstName} {LastName}, Age: {Age}, Phone: {Phone}, Email: {Email}";
     }
 }
---
+++ HotelReservationSystem/ReservationManagementSystem.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SunsetHotelReservationSystem
+{
+    class ReservationManagementSystem
+    {
+        private List<Reservation> reservations;
+
+        public ReservationManagementSystem()
+        {
+            reservations = new List<Reservation>();
+        }
+
+        public void CreateReservation(Guest guest, Room room, DateTime checkInDate, DateTime checkOutDate)
+        {
+            int reservationID = GenerateReservationID();
+            Reservation newReservation = new Reservation(reservationID, guest, room, checkInDate, checkOutDate);
+
+            // Check if the room is available for the specified dates
+            if (IsRoomAvailable(room, checkInDate, checkOutDate))
+            {
+                reservations.Add(newReservation);
+                Console.WriteLine($"Reservation successful! Reservation ID: {reservationID}");
+            }
+            else
+            {
+                Console.WriteLine("Sorry, the selected room is not available for the specified dates.");
+            }
+        }
+
+        public void ModifyReservation(int reservationID, DateTime newCheckInDate, DateTime newCheckOutDate)
+        {
+            Reservation reservation = FindReservation(reservationID);
+
+            if (reservation != null)
+            {
+                // Check if the modified dates are valid
+                if (IsRoomAvailable(reservation.Room, newCheckInDate, newCheckOutDate))
+                {
+                    reservation.CheckInDate = newCheckInDate;
+                    reservation.CheckOutDate = newCheckOutDate;
+                    Console.WriteLine($"Reservation {reservationID} modified successfully!");
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, the modified dates conflict with an existing reservation.");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Reservation with ID {reservationID} not found.");
+            }
+        }
+
+        public void CancelReservation(int reservationID)
+        {
+            Reservation reservation = FindReservation(reservationID);
+
+            if (reservation != null)
+            {
+                reservations.Remove(reservation);
+                Console.WriteLine($"Reservation {reservationID} canceled successfully!");
+            }
+            else
+            {
+                Console.WriteLine($"Reservation with ID {reservationID} not found.");
+            }
+        }
+
+        private int GenerateReservationID()
+        { 
+
+            return reservations.Count + 1;
+        }
+
+        private bool IsRoomAvailable(Room room, DateTime checkInDate, DateTime checkOutDate)
+        {
+            // Check if the room is available for the specified dates
+            foreach (Reservation reservation in reservations)
+            {
+                if (reservation.Room == room &&
+                    (checkInDate < reservation.CheckOutDate && checkOutDate > reservation.CheckInDate))
+                {
+                    return false; // Room is not available
+                }
+            }
+            return true; // Room is available
+        }
+
+        private Reservation FindReservation(int reservationID)
+        {
+            return reservations.Find(reservation => reservation.ReservationID == reservationID);
+        }
+    }
+}
+

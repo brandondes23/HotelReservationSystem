9a3385aed3c19b911043db64b8b3e19927875751
brandondes23 <152325009+brandondes23@users.noreply.github.com>
2023-12-12 22:05:36 -0600

    Update Reservation.cs

--- HotelReservationSystem/Reservation.cs
+++ HotelReservationSystem/Reservation.cs
@@ -3,19 +3,19 @@ namespace HotelReservationSystem
 {
     record Reservation(int ReservationID, string FirstName, string LastName, string Email)
     {
-        private readonly global::System.Int32 age;
+        private readonly global::System.Int32 age; //private fields for age and phone
         private readonly global::System.String phone;
 
         public System.Int32 Age => age;
 
         public System.String Phone => phone;
-
+        //constructor for creating reservation instances, made up of reservationID, firstName, lastName, age, phone, email
         public Reservation(int reservationID, string firstName, string lastName, int age, string phone, string email) : this(reservationID, firstName, lastName, email)
         {
-            SetAge(age);
-            SetPhone(phone);
+            SetAge(age); //call private methods 
+            SetPhone(phone); 
         }
-
+        // Override the ToString method to provide a human-readable string representation of the object
         public override string ToString => $"Reservation ID: {ReservationID}, Guest: {FirstName} {LastName}, Age: {Age}, Phone: {Phone}, Email: {Email}";
     }
 }
